df47b743369703072dfcbe576af3ee95d317963c
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-04 00:34:10 +0200

    fix(statuses): missing seeder

---
+++ database/seeders/StatusesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Status;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('statuses')->insert([
+            [
+                'id' => Status::PENDING->value,
+                'name' => Status::PENDING->name
+            ],
+            [
+                'id' => Status::DECLINED->value,
+                'name' => Status::DECLINED->name
+            ],
+            [
+                'id' => Status::APPROVED->value,
+                'name' => Status::APPROVED->name
+            ]
+        ]);
+    }
+}
