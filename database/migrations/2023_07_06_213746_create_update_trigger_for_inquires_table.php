82c75d25162bee25a7cfbf78139b6c0a7bb58a91
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-06 23:39:12 +0200

    feat: trigger on inquire vacation days not tested yet

---
+++ database/migrations/2023_07_06_213746_create_update_trigger_for_inquires_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+            CREATE OR REPLACE FUNCTION update_vacation_days()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                IF NEW.status_id = 3 AND NEW.type = 1 THEN
+                    UPDATE company_employees
+                    SET vacation_days = vacation_days - (NEW."end" - NEW.start)
+                    WHERE user_id = NEW.user_id;
+                END IF;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+        ');
+
+        DB::unprepared('
+            CREATE TRIGGER inquires_after_update
+            AFTER UPDATE ON inquires
+            FOR EACH ROW
+            EXECUTE FUNCTION update_vacation_days();
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS inquires_after_update ON inquires');
+        DB::unprepared('DROP FUNCTION IF EXISTS update_vacation_days()');
+    }
+};
