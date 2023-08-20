a7738988b839eac670b834db9d318f657434bd7e
nikoladomazetovikj <nikolad150@gmail.com>
2023-08-20 21:11:43 +0200

    chore: app name

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'EmployeeSystemManager'),
+    'name' => env('APP_NAME', 'Employee System Manager'),
 
     /*
     |--------------------------------------------------------------------------
