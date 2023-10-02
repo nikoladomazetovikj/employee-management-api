59ff7c6e60fba5d9af10f6154a88c19b52d6a297
nikoladomazetovikj <nikolad150@gmail.com>
2023-10-02 14:13:16 +0200

    fix: verify email link

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,17 @@ class AuthServiceProvider extends ServiceProvider
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        VerifyEmail::createUrlUsing(function ($notifiable) {
+            $baseURL = config('app.frontend_url');
+            $key = $notifiable->getKey();
+            $emailHash = sha1($notifiable->getEmailForVerification());
+
+            $signedUrl = URL::temporarySignedRoute(
+                'verification.verify', now()->addMinutes(60), ['id' => $key, 'hash' => $emailHash], absolute: false);
+
+            return $baseURL.$signedUrl;
         });
 
         //
