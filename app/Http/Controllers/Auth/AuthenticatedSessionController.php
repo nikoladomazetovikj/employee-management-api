4766c51288b5a32061740e87662d8739a4eaf651
nikoladomazetovikj <nikolad150@gmail.com>
2023-10-06 09:33:57 +0200

    fix: user login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\Role;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
@@ -21,7 +22,15 @@ class AuthenticatedSessionController extends Controller
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
-        return response()->json(['token' => $token, 'user' => $request->user()], 200);
+        $user = $request->user();
+        $company = $request->user()->company;
+
+        foreach ($company as $c) {
+            $roleId = $c->pivot->role_id;
+            $role = Role::getDescription($roleId);
+        }
+
+        return response()->json(['token' => $token, 'user' => $request->user(), 'role' => $role], 200);
     }
 
     /**
