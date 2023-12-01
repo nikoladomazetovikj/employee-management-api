9b144321a03694f77f4f1b0735fada56dc2be487
nikoladomazetovikj <nikolad150@gmail.com>
2023-12-01 12:17:21 +0100

    config: default app name

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Employee System Manager'),
+    'name' => env('APP_NAME', 'Time Mozaic'),
 
     /*
     |--------------------------------------------------------------------------
